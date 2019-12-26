d52c7f2783f9597f8f5277fc13ded6ef8f56c33f
maximato <maxxxnes@gmail.com>
2019-12-26 14:17:12 +0300

    Fix comment in test_items.py, comment sleep(10)

--- test_items.py
+++ test_items.py
@@ -6,8 +6,8 @@ def test_existence_of_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
 
-    # visual test
-    time.sleep(10)
+    # for visual test
+    # time.sleep(10)
 
     # find button 'add to basket'
     btn = browser.find_element_by_css_selector("button.btn-add-to-basket")
